20c9bdd510b8c53a18003abaafc0eae454c44d6a
Taynah-M-Alves <taynahdemichelialves@gmail.com>
2025-08-30 22:32:02 -0400

    Criação da tabela StatusEvento e Evento

--- backend/evento/admin.py
+++ backend/evento/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import statusEvento, evento
 
-# Register your models here.
+admin.site.register(statusEvento)
+admin.site.register(evento)
--- backend/evento/models.py
+++ backend/evento/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class statusEvento(models.Model):
+    NomeStatusEvento = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.NomeStatusEvento
+    
+
+class evento(models.Model):
+    titulo = models.CharField(max_length=50)
+    descricao = models.TextField()
+    prazo = models.DateField()
+    dataCriacao = models.DateTimeField(auto_now_add=True)
+    statusEvento = models.ForeignKey(statusEvento, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Titulo: {self.titulo} -> Prazo: {self.prazo}"
