d6e4d92bffc944e7147c53aaffc8498da8ad4151
Taynah-M-Alves <taynahdemichelialves@gmail.com>
2025-11-30 20:28:14 -0400

    add status de atrasado automaticamente

--- backend/tarefa/views.py
+++ backend/tarefa/views.py
@@ -4,6 +4,7 @@ from .models import Tarefa, StatusTarefa
 from grupo.models import Grupo
 from usuarios.models import AlunoProfile
 from historico.models import historico, tipoAlteracao
+from datetime import date
 from django.views.decorators.csrf import csrf_exempt
 import json
 
@@ -72,6 +73,22 @@ def ver_tarefa_por_grupo(request,id):
         received_grupo = get_object_or_404(Grupo, pk=id)
         tarefas = Tarefa.objects.filter(grupo = received_grupo)
 
+        status_atrasada = get_object_or_404(StatusTarefa, nome_status="Atrasado")
+
+        for t in tarefas:
+            if ( t.status_tarefa.nome_status == "Pendente" and t.prazo < date.today()):
+                    atrasado = get_object_or_404(tipoAlteracao, pk=4)
+
+                     # Cria histórico somente quando status REALMENTE muda
+                    historico.objects.create(
+                        tipo_alteracao=atrasado,
+                        tarefa_alterada=t,
+                        data_alteracao=date.today(),
+                    )
+
+                    t.status_tarefa = status_atrasada
+                    t.save()
+
         task_list = [{"id":t.id, 
                       "titulo": t.titulo, 
                       "descricao": t.descricao,
@@ -90,19 +107,44 @@ def ver_tarefa_por_grupo(request,id):
 def definir_tarefa_como_finalizada(request,id):
     if request.method == 'PATCH':
         received_tarefa = get_object_or_404(Tarefa, pk=id)
-        status = get_object_or_404(StatusTarefa, pk=2)
+        status_pendente = get_object_or_404(StatusTarefa, nome_status='Pendente')
+        status_atrasado = get_object_or_404(StatusTarefa, nome_status='Atrasado')
+        status_vencido = get_object_or_404(StatusTarefa, nome_status='Vencido')
 
-        received_tarefa.status_tarefa = status
-        received_tarefa.save()
+        if received_tarefa.status_tarefa == status_pendente:
+            status = get_object_or_404(StatusTarefa, pk=2)
 
-        tipo_criacao_tarefa = get_object_or_404(tipoAlteracao, pk=3)
+            received_tarefa.status_tarefa = status
+            received_tarefa.save()
 
-        alteracao = historico.objects.create(
-            tipo_alteracao= tipo_criacao_tarefa,
-            tarefa_alterada = received_tarefa if received_tarefa else None,
-            )
+            tipo_criacao_tarefa = get_object_or_404(tipoAlteracao, pk=3)
 
-        return JsonResponse({"tarefa":alteracao.tarefa_alterada.titulo,"status_tarefa":received_tarefa.status_tarefa.nome_status}, status=200, safe=False)
 
-    else:
-        return JsonResponse({"erro": "Método não permitido. Use o método GET para ver todas as Tarefas e POST para criar uma Tarefa."}, status=405)
+            alteracao = historico.objects.create(
+                tipo_alteracao= tipo_criacao_tarefa,
+                tarefa_alterada = received_tarefa if received_tarefa else None,
+                )
+
+            return JsonResponse({"tarefa":alteracao.tarefa_alterada.titulo,"status_tarefa":received_tarefa.status_tarefa.nome_status}, status=200, safe=False)
+
+        if received_tarefa.status_tarefa == status_atrasado:
+            try:
+
+                received_tarefa.status_tarefa = status_vencido
+                received_tarefa.save()
+
+                tipo_criacao_tarefa = get_object_or_404(tipoAlteracao, pk=5)
+
+
+                alteracao = historico.objects.create(
+                    tipo_alteracao= tipo_criacao_tarefa,
+                    tarefa_alterada = received_tarefa if received_tarefa else None,
+                    )
+
+                return JsonResponse({"tarefa":alteracao.tarefa_alterada.titulo,"status_tarefa":received_tarefa.status_tarefa.nome_status}, status=200, safe=False)
+        
+            except Exception as e:
+                return JsonResponse({"erro": str(e)}, status=400)
+
+        else:
+            return JsonResponse({"erro": "Método não permitido. Use o método GET para ver todas as Tarefas e POST para criar uma Tarefa."}, status=405)
